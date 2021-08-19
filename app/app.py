8893ab50395a20d5f5b88abeaff6541865b08a83
shannontull <85319975+shannontull@users.noreply.github.com>
2021-08-19 19:20:12 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -47,9 +47,41 @@ async def welcome(request: Request, db: Session=Depends(get_db)):
 
     fig10 = px.bar(dfteam, x='Team', y='Salary')
     fig10.update_layout( yaxis = dict( tickfont = dict(size=5)),
-    xaxis = dict( tickfont = dict(size=5)),
-    font=dict(size=5),
-    margin=dict(l=0, r=0, t=0, b=0))
+        xaxis = dict( tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
     team10 = fig10.to_html(full_html=False, include_plotlyjs='cdn')
 
-    return templates.TemplateResponse("chart.html", {"request": request, "top10":top10, "team10":team10})
+    dfteam=df.loc[df['Team'],isin(dfteam.Team)]
+    figtem=px.bar(dfteam, x='Team',y='Salary',color='Fieldposition')update_xaxes(categoryorder="total descending")
+    figteam.update_layout(yaxis=dict(tickfont=dict(size=5)))
+    xaxis=dict(tickfont=dict(size=5)),
+    font=dict(size=5),
+    margin=dict(l=0,r=0,t=0,b=0))
+    teamsalary=figteam.to_html(full_html=False, include_plotlyjs='cdn')
+    pos10=dfteam.groupby('Fieldposition')['Salary'].mean().sort_values(ascending=False).head(10)
+    pos10=pos10.reset_index()
+    figpos=px.box(dfteam.loc[dfteam['Fieldposition'].isin(pos10.fieldposition)],x='Fieldposition',y='Salary')
+    figpos.update_layout(yaxis=dict(tickfont=dict(size=5)),xaxis=dict(tickfont=dict(size5)),font=dict(size=5),margin=dict(l=0,r=0,t=0,b=0))
+    possalary=figpos.to_html(full_html=False,include_plotlyjs='cdn')
+
+    bottom10=df.groupby('Team')['Salary'].sum()
+    bottom10=bottom10.reset_index()
+    bottom10=dfteam.sort_values('Salary',ascending=False).tail(10)
+    dfteam=df,loc[df['Team'].isin(bottom10.Team)]
+
+    pos10=dfteam.groupby('Fieldposition')['Salary'].mean().sort_values(ascending=False).head(10)
+    pos10=pos10.reset_index()
+    figpos2=px.box(dfteam.loc[dfteam['Fieldpostion'].isin(pos10.Fieldpostion)],x='Fieldposition',y='Salary',color_discrete_squence=['red'])
+    figpos2.update_layout(yaxis=dict(tickfont=dict(size=5)),xaxis=dict(tickfont=dict(size=5)),font=dict(size=5),margin=dict(l=0,r=0,t=0,b=0))
+    possalary2=figpos2.to_html(full_html=False,include_plotlyjs='cdn')
+
+    dfteam=df.groupby('Fieldpostion')['Salary'].mean()
+    dfteam=dfteam.reset_index()
+    dfteam=dfteam.sort_values('Salary',ascending=False)
+
+    figpie=px.pie(dfteam,values='Salary', names='Fieldposition')
+    figpie.update_layout(yaxis=dict(tickfont=dict(size=5)),xaxis=dict(tickfont=dict(size=5)),font=dict(size=5),margin=dict(l=0,r=0,t=0,b=0))
+    pospie=figpie.to_html(full_html=False,include_plotjs='cdn')
+
+    return templates.TemplateResponse("chart.html", {"request": request, "top10":top10, "team10":team10,"teamsalary":teamsalary,"possalary":possalary,"possalary2":possalary2,"pospie":pospie})
